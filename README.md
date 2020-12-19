8b27e68745e22ff10aec7f864d32731940b3a07d
Amanda Crawford <crawfama@oregonstate.edu>
2020-12-18 18:54:26 -0800

    added example code

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # puiHw9
 Intro to API code for assignment
 
-This assignment is the code for the slides found here: https://docs.google.com/presentation/d/1V3svGYuDul_irvi_uRPr69qpmHjSNVOoLsNcm6ywoc4/edit?usp=sharing 
+This assignment is the code for the slides found here: https://docs.google.com/presentation/d/1V3svGYuDul_irvi_uRPr69qpmHjSNVOoLsNcm6ywoc4/edit?usp=sharing
 
 The code is meant to serve as a introduction to what is possible with APIs for those who have not yet learned much about them.
+However, there is no included API key for safety so in order to use with a public API, you must create an API key and follow the specific API instructions.
+
+
+
+## Dependencies
+
+This code requires Axios which can be installed with node by running: npm install axios
+
+This code is also designed as an example for possible API requests and does not connect to a specific API because I don't want a personal API key available for use.
---
+++ apiExploration.js
@@ -0,0 +1,153 @@
+//This line is REQUIRED for all Axios functions
+const axios = require('axios').default;
+
+/***********************************************
+*
+*       GET REQUESTS
+*
+***********************************************/
+
+//Basic Get for all products
+axios.get('/products')
+  .then(function (response) {
+    // handle success
+    console.log("GET /products succeeded.  Returned should be a 2xx code and all products.");
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log("GET /products failed. Received a 4xx or 5xx error code.");
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+   console.log("GET /products at then function. May now do anything you wish.");
+  });
+
+//Basic Get for all products that will return 404
+axios.get('/proudcts') //Product is misspelled
+  .then(function (response) {
+    // handle success - WILL NOT OCCUR
+    console.log("GET /products succeeded.  Returned should be a 2xx code and all products.");
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error - WILL OCCUR with 404 because of misspelled
+    console.log("GET /products failed. Received a 4xx or 5xx error code.");
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+   console.log("GET /products at then function. May now do anything you wish.");
+  });
+
+//Filtering all products for women's boots size 7
+axios.get('/products/women/boots/7')
+  .then(function (response) {
+    // handle success
+    console.log("GET /products/women/boots/7 succeeded.  Returned should be a 2xx code and all women's boots size 7.");
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log("GET /products/women/boots/7 failed. Received a 4xx or 5xx error code.");
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+   console.log("GET /products/women/boots/7 at then function. May now do anything you wish.");
+  });
+
+//Filtering can also be formatted like this:
+//This makes the parameters more clear and is eailsy done with variables from other areas of code
+axios.get('/products', {
+    params: {
+      gender: 'women',
+      shoeType: 'boots',
+      size: 7
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
+  /***********************************************
+  *
+  *       POST REQUESTS
+  *
+  ***********************************************/
+
+//Create a new user "Luke Skywalker"
+axios.post('/user', {
+  firstName: 'Luke',
+  lastName: 'Skywalker'
+})
+.then(function (response) {
+  console.log("POST /user succeeded at creating Luke Skywalker.  Returned should be a 201 status code.");
+  console.log(response);
+})
+.catch(function (error) {
+  console.log("POST /user failed at creating Luke Skywalker.  Returned should be a 500 or 400 status code.");
+  console.log(error);
+});
+
+
+//misspelled firstName will result in error
+axios.post('/user', {
+  firstNmae: 'Luke',
+  lastName: 'Skywalker'
+})
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+/***********************************************
+*
+*       PUT REQUESTS
+*
+***********************************************/
+
+//Update the user "Luke Skywalker" and add his favorite thing
+axios.post('/user', {
+  firstName: 'Luke',
+  lastName: 'Skywalker',
+  favoriteThing: 'His Light Saber'
+})
+.then(function (response) {
+  console.log("POST /user succeeded at upadting Luke Skywalker.  Returned should be a 201 status code.");
+  console.log(response);
+})
+.catch(function (error) {
+  console.log("POST /user failed at updating Luke Skywalker.  Returned should be a 500 or 400 status code.");
+  console.log(error);
+});
+
+
+  /***********************************************
+  *
+  *       Delete REQUESTS
+  *
+  ***********************************************/
+
+axios.delete('/product', {
+  id: 1458
+})
+.then(function (response) {
+  console.log("DELETE /product/1458 succeeded at deleting product with id 1458.  Returned should be a 200 status code.");
+  console.log(response);
+})
+.catch(function (error) {
+  console.log("DELETE /product/1458 failed at deleting product with id 1458.  Returned should be a 500 or 400 status code.");
+  console.log(error);
+});
